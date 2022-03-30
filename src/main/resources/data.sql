1b699c56f7e396b765a712a68c29597a75800d82
IBERMATICA\BICUGUAL <a.cuesta.gutierrez@ibermatica.com>
2022-03-30 09:35:22 +0200

    Preparacion para pruebas de accesibilidad

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-/*
-INSERT INTO t_persona (id, nombre, apellido) VALUES
-  ('1', 'John', 'Brown'),
-  ('2', 'Mary', 'Smith');
-  INSERT INTO t_alumno (identifier,name, surname) VALUES
-  ( 1,'Anne', 'Thomas'),
-  ( 2,'Michel', 'Black');
-*/
